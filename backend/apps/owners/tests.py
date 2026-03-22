ce606febf3de256f465ba3609a54bdfb4526fe9a
SkyRaks <nano.voloshin2016@gmail.com>
2026-03-22 19:11:55 -0400

    jwt auth

--- backend/apps/owners/tests.py
+++ backend/apps/owners/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from apps.owners.models import Owner
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from django.urls import reverse
 
 # Create your tests here.
 
@@ -18,3 +20,20 @@ class OwnerModelTest(TestCase):
 
         self.assertEqual(Owner.objects.count(), 1)
         self.assertEqual(owner.user.username, "oliver")
+    
+    def test_can_get_tokens(self):
+        user = User.objects.create_user(username="oliver", password="password123")
+        Owner.objects.create(user=user)
+
+        client = APIClient()
+        # token_obtain_pair
+        url = reverse("token_obtain_pair")
+
+        res = client.post(url, {
+            "username": "oliver",
+            "password": "password123",
+        }, format="json")
+
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("access", res.data)
+        self.assertIn("refresh", res.data)
